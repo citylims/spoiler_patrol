a3ee63c593ae6928770c1f5cb95a867646d76255
Ausin Formica <austin.formica@gmail.com>
2015-10-17 21:02:26 -0400

    popup get/set

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,4 +1,40 @@
-'use strict';
+var app = chrome.extension.getBackgroundPage();
+// console.log(app.backgroundFunction());
 
-console.log('\'Allo \'Allo! Popup');
-//# sourceMappingURL=popup.js.map
+
+document.body.onload = function() {
+  console.log("onload");
+  chrome.storage.sync.get('value', function(items) {
+    if (!chrome.runtime.error) {
+      console.log(items);
+      if ($.isEmptyObject(items)) {
+        console.log("empty");
+      }
+      else {
+        $('#spoilers').text(items);
+      }
+    }
+  })
+}
+
+$('#add-btn').click(function(e) {
+  e.preventDefault();
+  var str = console.log($('input').val());
+  if (str === "") {
+    console.log("break");
+    return;
+  }
+  else {
+    chrome.storage.sync.set({'value': str}, function() {
+           // Notify that we saved.
+           console.log('Settings saved');
+           window.close();
+     });
+   }
+});
+
+
+$('#get-btn').click(function(e) {
+  e.preventDefault();
+
+})
