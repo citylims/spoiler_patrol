1feeb0a0b7cf907348c53f5ec3ff6365838ed719
Ausin Formica <austin.formica@gmail.com>
2015-10-18 12:09:39 -0400

    rm logs

--- app/scripts/content.js
+++ app/scripts/content.js
@@ -5,13 +5,12 @@ function hoist() {
 var noSpoilers = function(obj) {
   var spoilers = obj.spoilers
   var count = 0;
+
   for (var i = 0; i < spoilers.length; i++) {
-    console.log(spoilers[i]);
     spoilerSearch(spoilers[i]);
   }
 
   function spoilerSearch(word) {
-    console.log(word);
     var str = word;
     if (str === '') {
       return;
@@ -19,19 +18,19 @@ var noSpoilers = function(obj) {
     var regex = new RegExp('\\b' + str + '\\b', 'gi');
     var matchRegex = $(document.body).text().match(regex);
     var matchCount = matchRegex ? matchRegex.length : 0;
-    console.log(matchCount);
     count += matchCount;
-  }
+  };
+
   console.log(count);
   chrome.runtime.sendMessage({spoilerCount: count}, function(response) {
     console.log(response.success);
   });
-}
+};
 
 function getArray() {
   chrome.storage.sync.get(["spoilers"], function(result) {
     noSpoilers(result);
   })
-}
+};
 
 hoist();
