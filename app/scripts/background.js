4b2ebc6e2eb7fbd59957386e079017867ce72416
Ausin Formica <austin.formica@gmail.com>
2015-10-18 12:00:52 -0400

    empty count handler

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -6,8 +6,12 @@ chrome.runtime.onInstalled.addListener(details => {
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     console.log(request.spoilerCount)
     if (request.spoilerCount) {
-      sendResponse({success: "updated count in background"});
       var display = request.spoilerCount.toString();
       chrome.browserAction.setBadgeText({text: display});
+      sendResponse({success: "page spoiler count " + display});
+    }
+    else {
+      chrome.browserAction.setBadgeText({text: "0"});
+      sendResponse({success: "no matched spoilers"})
     }
 });
