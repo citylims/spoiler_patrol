3ce1a87e65fa1ff5fa974e32671fd8b569cc93ab
Ausin Formica <austin.formica@gmail.com>
2015-10-17 21:37:43 -0400

    bind chrome storage to popup

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,40 +1,40 @@
 var app = chrome.extension.getBackgroundPage();
-// console.log(app.backgroundFunction());
-
 
 document.body.onload = function() {
   console.log("onload");
-  chrome.storage.sync.get('value', function(items) {
+  chrome.storage.sync.get('value', function(item) {
     if (!chrome.runtime.error) {
-      console.log(items);
-      if ($.isEmptyObject(items)) {
+      console.log(item);
+      if ($.isEmptyObject(item)) {
         console.log("empty");
       }
       else {
-        $('#spoilers').text(items);
+        updatePopup(item.value)
       }
     }
   })
 }
 
+
 $('#add-btn').click(function(e) {
   e.preventDefault();
-  var str = console.log($('input').val());
-  if (str === "") {
-    console.log("break");
-    return;
-  }
-  else {
+  var str = $('input').val();
+  if (str !== "") {
     chrome.storage.sync.set({'value': str}, function() {
-           // Notify that we saved.
-           console.log('Settings saved');
-           window.close();
-     });
-   }
+      updatePopup(str);
+      // window.close();
+    });
+    chrome.runtime.sendMessage({add: "add"}, function(response) {
+      console.log(response.farewell);
+      updatePopup(str)
+    });
+    chrome.runtime.sendMessage({greeting: "greeting"}, function(response) {
+      console.log(response.farewell);
+    });
+  }
 });
 
 
-$('#get-btn').click(function(e) {
-  e.preventDefault();
-
-})
+function updatePopup(str) {
+  $('#spoilers').text(str);
+}
