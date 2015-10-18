30490091d31e842c1055ea7ff07f912dc20ad63e
Ausin Formica <austin.formica@gmail.com>
2015-10-17 21:42:28 -0400

    popup listeners

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,15 +1,28 @@
-'use strict';
-
 chrome.runtime.onInstalled.addListener(details => {
   console.log('previousVersion', details.previousVersion);
 });
 
-//Listeners
+chrome.storage.sync.get(null, function(items) {
+  var allKeys = Object.keys(items);
+  console.log(allKeys);
+});
+
+//listen for spoiler count
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
   var display = request.message.toString();
   chrome.browserAction.setBadgeText({text: display});
 });
 
-chrome.browserAction.onClicked.addListener(function() {
-  //
+//listen for popup events
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.add) {
+    console.log("add spoiler");
+  }
+  else {
+    console.log("debug");
+  }
 });
+
+function backgroundFunction () {
+  return "backgroundFunction"
+}
