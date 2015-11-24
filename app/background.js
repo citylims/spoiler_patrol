d3c303a124a90ae5b64833a940290d6b55d4e369
Ausin Formica <austin.formica@gmail.com>
2015-11-23 20:20:43 -0500

    rm logs

--- app/background.js
+++ app/background.js
@@ -1,49 +1,50 @@
 chrome.runtime.onInstalled.addListener(details => {
-  console.log('previousVersion', details.previousVersion);
+  if (details.previousVersion) {
+    console.log(details.previousVersion);
+  }
 });
 
 //refresh for new tab
 chrome.tabs.onCreated.addListener(function(tabId, changeInfo, tab) {
-    refeshScript();
+  refeshScript();
 });
 
 //refresh for active tab
 chrome.tabs.onActivated.addListener(function(activeInfo) {
-    chrome.tabs.get(activeInfo.tabId, function (tab) {
-      refreshScript();
-    });
+  chrome.tabs.get(activeInfo.tabId, function (tab) {
+    refreshScript();
+  });
 });
 
 //refresh for current tab
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, updatedTab) {
-    chrome.tabs.query({'active': true}, function (activeTabs) {
-        var activeTab = activeTabs[0];
-        if (activeTab == updatedTab) {
-          refreshScript();
-        }
-    });
+  chrome.tabs.query({'active': true}, function (activeTabs) {
+    var activeTab = activeTabs[0];
+    if (activeTab === updatedTab) {
+      refreshScript();
+    }
+  });
 });
 
 //messge router
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.spoilerCount) {
-      console.log("spoilerCount:" + request.spoilerCount);
-      var display = request.spoilerCount.toString();
-      chrome.browserAction.setBadgeText({text: display});
-      sendResponse({success: "page spoiler count " + display});
-    } else {
-      chrome.browserAction.setBadgeText({text: "0"});
-      sendResponse({success: "no matched spoilers"});
-    }
-    if (request.refresh) {
-      sendResponse({success: "Success"});
-      refreshTabs();
-    }
+  if (request.spoilerCount) {
+    var display = request.spoilerCount.toString();
+    chrome.browserAction.setBadgeText({text: display});
+    sendResponse({success: "page spoiler count " + display});
+  } else {
+    chrome.browserAction.setBadgeText({text: "0"});
+    sendResponse({success: "no matched spoilers"});
+  }
+  if (request.refresh) {
+    sendResponse({success: "Success"});
+    refreshTabs();
+  }
 });
 
 function refreshScript() {
   chrome.tabs.executeScript({ file: "jquery.min.js" }, function() {
-      chrome.tabs.executeScript({ file: "scripts/content.js" });
+    chrome.tabs.executeScript({ file: "scripts/content.js" });
   });
 }
 
